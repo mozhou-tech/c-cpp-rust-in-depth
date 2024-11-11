9983c86c7604334e162223ac6a33968bcb85441a
liuyuancheng <tenstone@foxmail.com>
2024-11-11 11:35:27 +0800

    linkedlist

---
+++ datastructures/linkedlist/LinkedList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LiuYuancheng on 2024/11/11.
+//
+
+#include "LinkedList.h"
---
+++ datastructures/linkedlist/LinkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by LiuYuancheng on 2024/11/11.
+//
+
+#ifndef CPP_IN_DEPTH_LINKEDLIST_H
+#define CPP_IN_DEPTH_LINKEDLIST_H
+
+
+class LinkedList {
+
+};
+
+
+#endif //CPP_IN_DEPTH_LINKEDLIST_H
