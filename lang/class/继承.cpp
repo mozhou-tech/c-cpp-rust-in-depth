90a2ad50863614a393d8f0a2e4e332f1de6eff17
liuyuancheng <tenstone@foxmail.com>
2024-11-15 11:43:15 +0800

    1

--- lang/class/继承.cpp
+++ lang/class/继承.cpp
@@ -2,7 +2,7 @@
  * @Author: liuyuancheng tenstone@foxmail.com
  * @Date: 2024-11-15 10:42:15
  * @LastEditors: liuyuancheng tenstone@foxmail.com
- * @LastEditTime: 2024-11-15 11:37:19
+ * @LastEditTime: 2024-11-15 11:38:43
  * @FilePath: /c-cpp-rust-in-depth/lang/class/继承.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
