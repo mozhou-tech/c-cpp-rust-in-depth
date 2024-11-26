92f17436ea7ec197ff1ee661324f728084fd62f5
liuyuancheng <tenstone@foxmail.com>
2024-11-26 13:01:43 +0800

    添加gtest

--- conanfile.py
+++ conanfile.py
@@ -12,6 +12,9 @@ class ExampleRecipe(ConanFile):
         self.requires("openssl/3.3.2")
         self.requires("gtest/1.15.0")
         self.requires("cjson/1.7.18")
+        self.requires("quickjs/2024-01-13")
+        self.requires("paho-mqtt-c/1.3.13")
+        self.requires("libmodbus/3.1.10")
 
     def layout(self):
         cmake_layout(self)
